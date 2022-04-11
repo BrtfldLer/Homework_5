d623e88b9a482a694c3fd7dc2cbfeb194522fef4
Valeriia <bartfeld.ler@yandex.ru>
2022-04-11 20:36:01 +0200

    Task_36 done

---
+++ Task_36/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задача 36. Массив случайных чисел. Найти сумму элементов, стоящих на нечётных позициях
+int amount = new Random().Next(0, 10);
+int[] array = new int [amount];
+int len = array.Length;
+int count = 0;
+
+for (int i = 0; i<len; i++)
+{
+array[i] = new Random().Next(-10,10);
+Console.Write($"{array[i]} ");
+if (i % 2 != 0)
+{
+    count = count + array[i];
+}
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях в данном массиве, равна {count}");
