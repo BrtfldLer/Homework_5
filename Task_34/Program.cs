0940f37d58bb6fb0b120785fbd861233dc02fb8a
Valeriia <bartfeld.ler@yandex.ru>
2022-04-11 20:32:34 +0200

    Task_34 done

---
+++ Task_34/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 34. Массив из случайных положительных трёхзначных чисел. 
+// Показать количество чётных чисел
+
+int amount = new Random().Next(0, 15);
+int[] array = new int [amount];
+int len = array.Length;
+int count = 0;
+
+for (int i = 0; i<len; i++)
+{
+array[i] = new Random().Next(100,1000);
+Console.Write($"{array[i]} ");
+    if (array[i] % 2 == 0)
+    {
+    count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел в данном массиве равно {count}");
